d39fe03f6a4b075df41a9439e8829d3e138f9976
marcupmc <marc.gregoire1989@gmail.com>
2013-04-04 09:49:04 +0200

    -First Commit

---
+++ assets/www/js/authentification.js
@@ -0,0 +1,31 @@
+// a changer pour la mise en production
+// Mettre l'adresse du serveur
+var serviceURL = "http://10.0.2.2:8080/TestRest/rest/";
+
+// A Comprendre
+$(document).ready( function () { 
+	
+
+	$("#connexionForm").submit( function() {
+		var $inputs = $("#connexionForm").find("input, select, button, textarea");
+	    // serialize the data in the form
+	    var serializedData = $("#connexionForm").serialize();
+		// à la soumission du formulaire						 
+		$.ajax({ // fonction permettant de faire de l'ajax
+			type: "POST", // methode de transmission des données au fichier php
+			url: serviceURL+"authentification",// url du fichier du WS
+			data: serializedData, // données à transmettre
+			success: function(msg){ // si l'appel a bien fonctionné
+				if(msg=="ok") // si la connexion en php a fonctionnée
+				{
+					alert('Ok tu es connecté ! ');
+				}
+				else // si la connexion en php n'a pas fonctionnée
+				{
+					alert('Probleme ! ');// on affiche un message d'erreur dans le span prévu à cet effet
+				}
+			}
+		});
+		return false; // permet de rester sur la même page à la soumission du formulaire
+	});
+});
