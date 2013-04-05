5e02b8428daf3c07efe87609ec09e82551bb8ce3
marcupmc <marc.gregoire1989@gmail.com>
2013-04-05 11:27:10 +0200

    authentification ok TODO import pdf

--- assets/www/js/authentification.js
+++ assets/www/js/authentification.js
@@ -1,31 +1,32 @@
-// a changer pour la mise en production
-// Mettre l'adresse du serveur
+//a changer pour la mise en production
+//Mettre l'adresse du serveur
 var serviceURL = "http://10.0.2.2:8080/TestRest/rest/";
 
-// A Comprendre
-$(document).ready( function () { 
-	
 
+$(document).ready( function () { 
 	$("#connexionForm").submit( function() {
 		var $inputs = $("#connexionForm").find("input, select, button, textarea");
-	    // serialize the data in the form
-	    var serializedData = $("#connexionForm").serialize();
+		// serialize the data in the form
+		var serializedData = $("#connexionForm").serialize();
 		// à la soumission du formulaire						 
 		$.ajax({ // fonction permettant de faire de l'ajax
 			type: "POST", // methode de transmission des données au fichier php
 			url: serviceURL+"authentification",// url du fichier du WS
 			data: serializedData, // données à transmettre
+			datatype:"string",
 			success: function(msg){ // si l'appel a bien fonctionné
-				if(msg=="ok") // si la connexion en php a fonctionnée
-				{
-					alert('Ok tu es connecté ! ');
-				}
-				else // si la connexion en php n'a pas fonctionnée
-				{
-					alert('Probleme ! ');// on affiche un message d'erreur dans le span prévu à cet effet
+				if(msg=="error") 
+					alert('Veuillez entrer un identifiant et un mot de passe valide');
+				else{
+					alert('Authentification réussie ! identifiant : '+msg)
+					document.location.href="home.html?id="+msg;
 				}
 			}
 		});
+		
 		return false; // permet de rester sur la même page à la soumission du formulaire
 	});
 });
+
+
+
